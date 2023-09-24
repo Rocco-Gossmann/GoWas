efb31ee7d67ac93778f41a0a80ce6e6876871508
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-09-24 04:55:10 +0200

    Started work on Tilemaps/Text implementation

--- project-template/scenes/debug.go
+++ project-template/scenes/debug.go
@@ -10,6 +10,7 @@ import (
 
 type debugScene struct {
 	cursorBMP canvas.Bitmap
+	fontBMP   canvas.Bitmap
 }
 
 //func (s debugScene) Tick(e *GoWas.Engine, dt float64) bool {
@@ -24,6 +25,9 @@ func (s debugScene) Load(e *GoWas.Engine) {
 
 func (s debugScene) Draw(e *GoWas.Engine, ca *GoWas.EngineCanvas) {
 	ca.FillColorA(0x00333333, 0x10, GoWas.CANV_CL_ALL)
+
+	ca.BlitBitmap(&(s.fontBMP), 0, 0, 0xff, GoWas.CANV_CL_NONE)
+
 	mouse := ca.Mouse
 	if mouse.X > 0 || mouse.Y > 0 {
 		ca.BlitBitmap(&(s.cursorBMP), int32(mouse.X), int32(mouse.Y), 0xff, GoWas.CANV_CL_NONE)
@@ -32,4 +36,5 @@ func (s debugScene) Draw(e *GoWas.Engine, ca *GoWas.EngineCanvas) {
 
 var Debug = debugScene{
 	cursorBMP: bmps.CursorBMP,
+	fontBMP:   bmps.AsciiFontBMP,
 }
