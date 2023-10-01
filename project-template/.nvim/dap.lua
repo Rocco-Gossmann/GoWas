9aa79cfd3aad2d22c923be05a1314fde18a9c93a
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-10-01 18:24:32 +0200

    .nvim config optimization

--- project-template/.nvim/dap.lua
+++ project-template/.nvim/dap.lua
@@ -1,12 +1,16 @@
 local dap = require("dap")
 
-dap.configurations.go = {
-	{
-		request = "launch",
-		type = "chrome",
-		url = "http://localhost:7353",
-		webRoot = "${workspaceFolder}",
-		runtimeExecutable = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
-		userDataDir = true,
-	},
-}
+local filetypes = { "javascript", "html", "go" }
+
+for _, filetype in pairs(filetypes) do
+	dap.configurations[filetype] = {
+		{
+			request = "launch",
+			type = "chrome",
+			url = "http://localhost:7353",
+			webRoot = "${workspaceFolder}",
+			runtimeExecutable = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
+			userDataDir = true,
+		},
+	}
+end
