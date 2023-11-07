f692a5b84fe9ffa315ec85e35c43ab5bbcf3de4d
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-11-07 06:50:51 +0100

    Update Project-Template main with new Options

--- project-template/main.go
+++ project-template/main.go
@@ -11,6 +11,9 @@ func main() {
 	e := GoWas.Init(GoWas.EngineSetup{
 		WindowWidth:  160,
 		WindowHeight: 144,
+
+		AutoClearPixels: true,
+		AutoClearColor:  0x00333333,
 	})
 
 	e.Run(&scenes.Debug)
